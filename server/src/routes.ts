b245a4a5fff40f37e48391d2ffbc19449c613a3b
karlos-silva <karlossilva194@gmail.com>
2020-06-06 16:55:09 -0300

    end back-end

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,17 @@
 
 import express from 'express';
 
-const routes = express.Router();
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
-routes.get('/', (request, response) => {
-  return response.json({message: 'Hello World'})
-});
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create); 
+routes.get('/points/:id', pointsController.show)
+routes.get('/points', pointsController.index); 
 
 export default routes;
